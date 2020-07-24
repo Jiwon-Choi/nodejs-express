0c665e30c410e543bf4f747ccab65e604b1e2b3a
Jiwon Choi <jwchoi.py@gmail.com>
2020-07-24 18:00:22 +0900

    2020-07-24

---
+++ syntax/object.js
@@ -0,0 +1,17 @@
+var members = ['egoing', 'k8805', 'hoya'];
+// console.log(members[1]);
+var i = 0;
+while (i < members.length) {
+    console.log(members[i]);
+    i += 1;
+}
+
+var roles = {
+    'programmer' : 'egoing', 
+    'designer' : 'k8805', 
+    'manager' : 'hoya'
+};
+// console.log(roles.designer);
+for (var key in roles) {
+    console.log(key, roles[key]);
+}
